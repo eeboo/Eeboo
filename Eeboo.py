571683d32be8b15021e7e7c4285b5ee9bc4fbd67
eeboo <yuh.nakata@gmail.com>
2015-07-03 00:56:03 +0200

    update

--- Eeboo.py
+++ Eeboo.py
@@ -10,21 +10,49 @@ from _Framework.ButtonMatrixElement import ButtonMatrixElement
 #DRUM_NOTES = (41, 42, 43, 44, 45, 46, 47, 57, 58, 59, 60, 61, 62, 63, 73, 74, 75, 76, 77, 78, 79, 89, 90, 91, 92, 93, 94, 95, 105, 106, 107)
 
 class Eeboo(ControlSurface):
-    """ Script for Eeeboo's Controller """
+    """ Script for Eeboo's Controller """
 
     def __init__(self, c_instance):
         ControlSurface.__init__(self, c_instance)
-        self.log_message("Hello world!")
+        self._clip = None
+        self.log_message(" ------ ")
         self.show_message("Hack enabled! Let's get groove!")
-        self.song().view.add_detail_clip_listener(self.on_selected_clip)
+        self.song().view.add_detail_clip_listener(self._on_selected_clip)
 
-    def on_selected_clip(self):
-        _selected_clip = self.song().view.detail_clip
-        if _selected_clip.name != None:
-            _name = _selected_clip.name
-        if _name != None:
-            self.show_message(_selected_clip.name)
-            self.log_message(_selected_clip.name)
+    
+    def _on_selected_clip(self):
+
+        self.log_message(" _on_selected_clip ")
+
+        # add_detail_clip_listener is triggered even slot is empty
+        if self.song().view.detail_clip != None:
+
+            # remove listener
+            if self._clip != None:
+                if self._clip.playing_status_has_listener(self._on_playing_status_changed):
+                    self._clip.remove_playing_status_listener(self._on_playing_status_changed)
+                if self._clip.playing_position_has_listener(self._on_playing_position_changed):
+                    self._clip.remove_playing_position_listener(self._on_playing_position_changed)
+
+
+            # new value
+            self._clip = self.song().view.detail_clip
+
+            # add listener
+            if self._clip.is_midi_clip:
+                self._clip.add_playing_status_listener(self._on_playing_status_changed)
+                self._clip.add_playing_position_listener(self._on_playing_position_changed)
+
+
+    def _on_playing_status_changed(self):
+        self.log_message( " on_playing_status_changed: "  )
+        if self._clip.is_playing:
+            self.show_message("clip playing")
+
+    def _on_playing_position_changed(self):
+        self.log_message( " _on_playing_position_changed: "  )
+        self.log_message( self._clip.playing_position )
+
 
     def _playing_clip_slot(self):
         track = self.song().view.selected_track
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Ableton live Remote script exploration!
 rsync -av --exclude='.git'  ~/Documents/workspace/Eeboo /Applications/Ableton\ Live\ 9\ Trial.app/Contents/App-Resources/MIDI\ Remote\ Scripts
 
 
-rsync -av --exclude='.git'  ~/workspace/Eeboo /Applications/Ableton\ Live\ 9\ Suite.app/Contents/App-Resources/MIDI\ Remote\ Scripts
+rsync -av --exclude={.git,.idea}   ~/workspace/Eeboo /Applications/Ableton\ Live\ 9\ Suite.app/Contents/App-Resources/MIDI\ Remote\ Scripts
